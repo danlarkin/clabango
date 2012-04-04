b55384978335bd0e56210b3e35fcb1e41ac3d65a
Dan Larkin <dan@danlarkin.org>
2012-04-04 11:58:27 -0400

    add a big 'ol test for include

--- test/clabango/test/parser.clj
+++ test/clabango/test/parser.clj
@@ -1,6 +1,7 @@
 (ns clabango.test.parser
   (:use [clojure.test]
-        [clabango.parser]))
+        [clabango.parser]
+        [clabango.tags]))
 
 (deftest lex-tests
   (is (= (lex "a b c d")
@@ -44,6 +45,48 @@
     (is (= s (render s {}))))
   (let [s "{a b c }\n d"]
     (is (= s (render s {})))))
+
+(deftest test-include
+  (let [s (str "{{foo}}\n dogs live in the park."
+               " {% include \"clabango/templates/foo.html\" %}")]
+    (is (= (parse s {:foo 42 :name "Dan"})
+           [{:type :string
+             :body {:token "42"
+                    :offset 3
+                    :line 1
+                    :file "UNKNOWN"}}
+            {:type :string
+             :body {:token "\n"
+                    :offset 8
+                    :line 1
+                    :file "UNKNOWN"}}
+            {:type :string
+             :body {:token " dogs live in the park. "
+                    :offset 1
+                    :line 2
+                    :file "UNKNOWN"}}
+            {:type :string
+             :body {:token "Hello, "
+                    :offset 1
+                    :line 1
+                    :file (load-template "clabango/templates/foo.html")}}
+            {:type :string
+             :body {:token "Dan"
+                    :offset 10
+                    :line 1
+                    :file (load-template "clabango/templates/foo.html")}}
+            {:type :string
+             :body {:token "!"
+                    :offset 16
+                    :line 1
+                    :file (load-template "clabango/templates/foo.html")}}
+            {:type :string
+             :body {:token "\n"
+                    :offset 17
+                    :line 1
+                    :file (load-template "clabango/templates/foo.html")}}]))
+    (is (= (render s {:foo 42 :name "Dan"})
+           "42\n dogs live in the park. Hello, Dan!\n"))))
 
 (deftest filter-upper
   (is (= "FOO" (render "{{f|upper}}" {:f "foo"}))))
