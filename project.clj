5dd91bd838a322c177c76a76d9d4c748a5506f0c
Dan Larkin <dan@danlarkin.org>
2012-11-13 12:05:47 -0500

    release 0.3

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clabango "0.3-SNAPSHOT"
-  :description "templating language?"
+(defproject clabango "0.3"
+  :description "A templating language?"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [cheshire "4.0.4"]
                  [commons-codec "1.6"]
