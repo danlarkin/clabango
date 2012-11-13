30a42cd40c0c480deb56ce0e1e72c26f4a3855af
Dan Larkin <dan@danlarkin.org>
2012-11-13 11:53:40 -0500

    bump to a newer version of cheshire

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clabango "0.3-SNAPSHOT"
   :description "templating language?"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [cheshire "3.1.0"]
+                 [cheshire "4.0.4"]
                  [commons-codec "1.6"]
                  [joda-time "2.1"]])
