4a720d7e97371f9141ad9cf26e17440ce74d0e19
Dan Larkin <dan@danlarkin.org>
2013-03-07 14:16:21 -0500

    clean up the if/else checking logic

--- src/clabango/tags.clj
+++ src/clabango/tags.clj
@@ -64,33 +64,20 @@
 
                               :default (throw (Exception. (str "Syntax error: "
                                                                if-node))))]
-    {:nodes (cond
-             (flip (context-lookup context decision))
-             (take-while #(not= "else" (:tag-name %)) body-nodes)
-
-             (some #{"else"} (map :tag-name body-nodes))
-             (rest (drop-while #(not= "else" (:tag-name %)) body-nodes))
-
-             :else [{:body (dissoc (:body if-node) :token)
-                     :type :noop}])}))
+    {:nodes (if (flip (context-lookup context decision))
+              (take-while #(not= "else" (:tag-name %)) body-nodes)
+              (rest (drop-while #(not= "else" (:tag-name %)) body-nodes)))}))
 
 (deftemplatetag "ifequal" "endifequal" [nodes context]
   (let [if-node (first nodes)
         operands (:args if-node)
         body-nodes (rest (butlast nodes))]
-    {:nodes (cond
-             (apply = (for [op operands]
-                        (if (= \" (.charAt op 0))
-                          (subs op 1 (dec (count op)))
-                          (context-lookup context op))))
-             (take-while #(not= "else" (:tag-name %)) body-nodes)
-
-             (some #{"else"} (map :tag-name body-nodes))
-             (rest (drop-while #(not= "else" (:tag-name %)) body-nodes))
-
-             :else
-             [{:body (dissoc (:body if-node) :token)
-               :type :noop}])}))
+    {:nodes (if (apply = (for [op operands]
+                           (if (= \" (.charAt op 0))
+                             (subs op 1 (dec (count op)))
+                             (context-lookup context op))))
+              (take-while #(not= "else" (:tag-name %)) body-nodes)
+              (rest (drop-while #(not= "else" (:tag-name %)) body-nodes)))}))
 
 (deftemplatetag "for" "endfor" [nodes context]
   (let [for-node (first nodes)
