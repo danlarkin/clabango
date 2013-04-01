3ccc45ade0714a9bbfa7d24ba44cd38026c62d71
Yogthos <dmitri.sotnikov@gmail.com>
2013-04-01 11:56:56 -0400

    added more comprehensive benchmark templates

--- project.clj
+++ project.clj
@@ -11,6 +11,8 @@
                  [net.sf.opencsv/opencsv "2.3"]
                  [criterium "0.3.1" :scope "test"]
                  [org.apache.commons/commons-lang3 "3.1"]]
-  :test-selectors {:default (fn [m] (not (:benchmarks m)))
-                   :benchmarks :benchmarks
+  :test-selectors {:default (fn [{:keys [element-benchmarks site-benchmarks]}] 
+                              (not (or element-benchmarks site-benchmarks)))
+                   :element-benchmarks :element-benchmarks
+                   :site-benchmarks    :site-benchmarks
                    :all (fn [m] true)})
